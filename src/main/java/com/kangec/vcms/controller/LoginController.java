0334e6a5a3c2bcea1814200c141a6a85a25e2a73
Ardient <ardien@126.com>
2020-10-17 21:06:20 +0800

    AOP日志管理和权限控制

---
+++ src/main/java/com/kangec/vcms/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.kangec.vcms.controller;
+
+import com.kangec.vcms.utils.logging.annotation.Log;
+import com.kangec.vcms.utils.logging.annotation.LogType;
+import org.springframework.boot.logging.LogLevel;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @Author kangec 10/16/2020 1:45 PM
+ * @Email ardien@126.com
+ **/
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/hello")
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN','ROLE_DRIVER')")
+    @Log(detail = "测试", level = LogLevel.INFO, type = LogType.SELECT)
+    public void hello(HttpSession session) {
+
+    }
+}
---
+++ src/main/java/com/kangec/vcms/filter/JwtLoginFilter.java
@@ -0,0 +1,46 @@
+package com.kangec.vcms.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kangec.vcms.entity.SysUser;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+
+public class JwtLoginFilter extends AbstractAuthenticationProcessingFilter {
+    protected JwtLoginFilter(String defaultFilterProcessesUrl, AuthenticationManager authenticationManager) {
+        super(defaultFilterProcessesUrl);
+        setAuthenticationManager(authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse httpServletResponse)
+            throws AuthenticationException, IOException, ServletException {
+        SysUser sysUser = new ObjectMapper().readValue(request.getInputStream(), SysUser.class);
+        return getAuthenticationManager()
+                .authenticate(new UsernamePasswordAuthenticationToken(sysUser.getName(), sysUser.getPassword()));
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response
+                                    , FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        Collection<? extends GrantedAuthority> authorities = authResult.getAuthorities();
+
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response
+                                                , AuthenticationException failed) throws IOException, ServletException {
+        super.unsuccessfulAuthentication(request, response, failed);
+    }
+}
