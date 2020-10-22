980a9d3a63255a27ea827fd8c00f409101eb08ec
kangec <ardien@126.com>
2020-10-22 21:45:50 +0800

    添加密码编码器

--- src/main/java/com/kangec/vcms/config/VcmsSecurity.java
+++ src/main/java/com/kangec/vcms/config/VcmsSecurity.java
@@ -1,21 +1,44 @@
 package com.kangec.vcms.config;
 
-import com.kangec.vcms.service.impl.VcmsUserDetailsService;
+import com.kangec.vcms.service.impl.VcmsUserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class VcmsSecurity extends WebSecurityConfigurerAdapter {
 
-    private VcmsUserDetailsService vcmsUserDetailsService;
+    private VcmsUserDetailsServiceImpl vcmsUserDetailsService;
+
+    /*
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 1.解决跨域问题。放行CORS预检
+        http.authorizeRequests().requestMatchers(CorsUtils::isPreFlightRequest).permitAll();
+
+        // 2.禁止Security创建HttpSession(Security会通过HttpSession获取SecurityContext)
+        http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().headers().cacheControl();
+
+        // 3.uri请求权限配置
+        http.authorizeRequests()
+                // 1) 放行注册请求
+                .antMatchers(HttpMethod.POST, "/user/register").permitAll();
+
+        // 4.拦截账号密码
+    }
+    */
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -33,12 +56,12 @@ public class VcmsSecurity extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) {
         web.ignoring().antMatchers("/js/**", "/css/**");
     }
 
     @Autowired
-    public void setVcmsUserDetailsService(VcmsUserDetailsService vcmsUserDetailsService) {
+    public void setVcmsUserDetailsService(VcmsUserDetailsServiceImpl vcmsUserDetailsService) {
         this.vcmsUserDetailsService = vcmsUserDetailsService;
     }
 }
