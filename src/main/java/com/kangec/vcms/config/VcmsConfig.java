1a67251abbe3376d1e2f3e6d033a0dda5343997f
kangec <ardien@126.com>
2020-10-24 16:02:07 +0800

    添加rememberMe

--- src/main/java/com/kangec/vcms/config/VcmsConfig.java
+++ src/main/java/com/kangec/vcms/config/VcmsConfig.java
@@ -1,8 +1,14 @@
 package com.kangec.vcms.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 /**
  * @author kangec
@@ -10,8 +16,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class VcmsConfig {
 
+    @Autowired
+    public DataSource dataSource;
+
     @Bean
     public BCryptPasswordEncoder getBCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public PersistentTokenRepository getPersistentTokenRepository() {
+        JdbcTokenRepositoryImpl repository = new JdbcTokenRepositoryImpl();
+        repository.setDataSource(dataSource);
+        // repository.setCreateTableOnStartup(true);
+        return repository;
     }
 }
--- src/main/java/com/kangec/vcms/config/VcmsSecurity.java
+++ src/main/java/com/kangec/vcms/config/VcmsSecurity.java
@@ -5,17 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class VcmsSecurity extends WebSecurityConfigurerAdapter {
 
     private VcmsUserDetailsServiceImpl vcmsUserDetailsService;
+    private PersistentTokenRepository persistentTokenRepository;
 
     /*
     @Override
@@ -35,6 +38,26 @@ public class VcmsSecurity extends WebSecurityConfigurerAdapter {
         // 4.拦截账号密码
     }
     */
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // 开启表单登陆
+        http.formLogin().and()
+                .authorizeRequests().anyRequest().authenticated();
+
+        // 开启记住密码
+        http.rememberMe()
+                .tokenRepository(persistentTokenRepository)
+                .tokenValiditySeconds(60)
+                .userDetailsService(vcmsUserDetailsService);
+
+        // 开启注销
+        http.logout().logoutSuccessUrl("/login");
+
+        // 关闭csrf
+        http.csrf().disable();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -59,5 +82,10 @@ public class VcmsSecurity extends WebSecurityConfigurerAdapter {
     @Autowired
     public void setVcmsUserDetailsService(VcmsUserDetailsServiceImpl vcmsUserDetailsServiceImpl) {
         this.vcmsUserDetailsService = vcmsUserDetailsServiceImpl;
+    }
+
+    @Autowired
+    public void setPersistentTokenRepository(PersistentTokenRepository persistentTokenRepository) {
+        this.persistentTokenRepository = persistentTokenRepository;
     }
 }
